36b34547b127c278b8f89bd3be7750105d2a26ff
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-13 20:55:21 -0300

    feat: criando rota para adicionar uma conta

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const {v4: uuidv4} = require('uuid');
+
 const app = express();
+
+const consumers = [];
 
 //definir os body params como json;
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Hello worlasdasdd ignite"
+//adicionar uma conta
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+    const uuid = uuidv4();
+
+    consumers.push({
+        cpf,
+        name,
+        uuid,
+        statement: []
     });
+
+    return response.status(201).send("Conta criada.");
 });
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
