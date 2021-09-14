1e770944a576072797bb82eda13b9f51e37cba70
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-14 10:10:51 -0300

    feat: permitir saque caso saldo for suficiente

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return response.status(400).json({error: 'Customer not found.'})
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((accumulator, operation) => {
+        if (operation.type === 'credit') {
+            return accumulator + operation.amount;
+        } else {
+            return accumulator - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 //adicionar uma conta
 app.post('/account', (request, response) => {
     const {cpf, name} = request.body;
@@ -46,6 +58,45 @@ app.post('/account', (request, response) => {
 app.get('/statement', verifyIfExistsAccountCPF, (request,response) => {
     const { customer } = request;
     return response.json(customer.statement);
+});
+
+//realizar um depósito em uma conta já existente
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const newStatementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit'
+    };
+
+    customer.statement.push(newStatementOperation);
+
+    return response.status(201).send();
+});
+
+//realizar saque em uma conta já existente
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.json({error: 'Você não possui saldo suficiente para realizar saque!'});
+    }
+
+    const newStatementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+    };
+
+    customer.statement.push(newStatementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
